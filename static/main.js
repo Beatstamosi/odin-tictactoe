d2a9a7992aa2ec691aa566a2d0afc5e4b1bb956e
Beatstamosi <moritz.lued@gmail.com>
2024-12-29 18:16:22 -0500

    Add pseudocode for game logic to main.js

---
+++ static/main.js
@@ -0,0 +1,45 @@
+// GameBoard Object
+    // define rows, columns
+    // create empty array for board
+
+    // fill array with values of 0
+
+    // getBoard function
+        // gets board array
+
+    // setToken function
+        // takes player and array index as input
+        // if value != 0, console.log error, return
+        // changes value to player
+
+    // printBoard function
+        // loops through board array
+        // console.log values of each index
+        // console.log borders
+
+    // return 3 functions
+
+
+// Player Object
+    // take playername as input; set default name
+    // create Player Array with name + token / sign
+    // return player Array
+
+
+// GameController Object
+    // initiate players
+    // initiate board via getBoard
+    // set active player
+    // switch player turn function
+    // get active player function
+    // print new round function
+    // playRound function
+        // setToken
+        // check for winner
+        // printBoard
+        // switch player turn
+        // printNewRound
+
+
+
+// ScreenController Object
