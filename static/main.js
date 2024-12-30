73c306a6d1eba8501d4bfc5774ab17396fbcef88
Beatstamosi <moritz.lued@gmail.com>
2024-12-30 18:06:36 -0500

    Add console playability & win condition: row

--- static/main.js
+++ static/main.js
@@ -7,6 +7,9 @@ const board = function() {
     // create array for board and fill with values of 0
     const board = Array.from({ length: rows}, () => Array(columns).fill(0));
 
+    // getBoard function
+    const getBoard = () => board;
+
     // setToken function
     const setToken = function (player, row, column) {
         // takes player and array index as input
@@ -28,6 +31,7 @@ const board = function() {
         
 
     return {
+        getBoard,
         setToken,
         printBoard
     }
@@ -57,18 +61,74 @@ function Players (playerOne = "Player 1", playerTwo = "Player 2") {
 
 
 // GameController Object
+function GameController () {
     // initiate players
-    // initiate board via getBoard
+    const players = Players().getPlayers();
+
     // set active player
+    let activePlayer = players[0];
+
     // switch player turn function
+    const switchPlayerTurn = function () {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
     // get active player function
+    const getActivePlayer = () => activePlayer;
+
     // print new round function
+    const printNewRound = function () {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn.`)
+    }
+
+
     // playRound function
+    const playRound = function (row, column) {
         // setToken
+        board.setToken(getActivePlayer().token, row, column);
+
         // check for winner
-        // printBoard
+        const boardArray = board.getBoard();
+        
+        // check for winner in row
+        const checkPlayer1Win = function (value) {
+            return value === "X"
+        }
+
+        const checkPlayer2Win = function (value) {
+            return value === "O"
+        }
+
+        boardArray.forEach(row => {
+            // check if winner in row
+
+            if (row.every(checkPlayer1Win)) {
+                console.log("Player 1 wins!")
+                return
+            } else if (row.every(checkPlayer2Win)) {
+                console.log("Player 2 wins!")
+                return
+            } 
+
+        })
+
+
         // switch player turn
+        switchPlayerTurn();
+
         // printNewRound
+        printNewRound();
+    }
+
+    printNewRound();
+
+    return {
+        playRound,
+    }
+        
+}
+    
 
 
 
